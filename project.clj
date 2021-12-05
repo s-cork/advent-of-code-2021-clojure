ba61665ee817a6434b119cae93ce363e5536f54d
Stu Cork <stu@anvil.works>
2021-12-05 22:32:05 +0800

    change init namespace

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.numeric-tower "0.0.5"]]
-  :repl-options {:init-ns advent-of-code.core}
+  :repl-options {:init-ns aoc.core}
   :plugins [[lein-cljfmt "0.8.0"]])
