888a43ddad9cf1ac72be738edfc83a2fdbc056e8
Stu Cork <stu@anvil.works>
2021-12-05 15:53:40 +0800

    tidy some names

--- src/advent_of_code/day5.clj
+++ src/advent_of_code/day5.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str]
             [advent-of-code.utils :refer [strings->ints split-comma]]))
 
-(def input "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2")
+;; (def input "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2")
 (def input (slurp (io/resource "input-day5.txt")))
 
 (defn line->coord-pair [line]
@@ -16,7 +16,7 @@
 
 (def coord-pairs (parse-input))
 
-(defn straight? [[[x1 y1] [x2 y2]]] (or (= x1 x2) (= y1 y2)))
+(defn h-or-v? [[[x1 y1] [x2 y2]]] (or (= x1 x2) (= y1 y2)))
 
 (defn dangerous? [[_coord coverage]] (> coverage 1))
 
@@ -35,5 +35,5 @@
        (filter dangerous?)
        count))
 
-(defn task1 [] (count-dangerous (filter straight? coord-pairs)))
+(defn task1 [] (count-dangerous (filter h-or-v? coord-pairs)))
 (defn task2 [] (count-dangerous coord-pairs))
