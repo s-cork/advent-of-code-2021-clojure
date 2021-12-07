b681d63e07f9977c7b39a0acc9dcfd3689cb6b53
Stu Cork <stu@anvil.works>
2021-12-07 21:39:24 +0800

    finish day1 2020

--- src/aoc/2020/day1.clj
+++ src/aoc/2020/day1.clj
@@ -2,21 +2,34 @@
   (:require [clojure.string :as str]
             [aoc.string-utils :refer [str->ints]]
             [aoc.file-utils :as file-utils]
-            [aoc.debug-utils :as debug]))
+            [aoc.debug-utils :refer [-log->]]))
 
 (def input (str->ints "1721\n979\n366\n299\n675\n1456"))
 (def input (file-utils/read-ints "2020/day1.txt"))
 
-(defn find-result [num-set]
-  (keep (fn [n]
-          (when (num-set (- 2020 n)) n)) num-set))
+(defn find-a-b [num-set]
+  (keep (fn [a]
+          (when-let [b (num-set (- 2020 a))]
+            [a b])) num-set))
 
-(defn do-multiply [n]
-  (* (- 2020 n) n))
+(defn find-a-b-c [num-set]
+  (for [a num-set b num-set
+        :when (not= a b)
+        :let [c (- 2020 a b)]
+        :when (num-set c)]
+    [a b c]))
 
-(comment
+(defn solve [finder]
   (->> input
        (apply hash-set)
-       find-result
+       finder
        first
-       do-multiply))
+       -log->
+       (apply *)))
+
+
+(comment
+  (solve find-a-b)
+  (solve find-a-b-c)
+  ;
+  )
